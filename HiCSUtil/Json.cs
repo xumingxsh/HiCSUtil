91d3f88b5f9bdbeb9c2c64bb463e3fef150ea10c
XuminRong <xumingxsh21@126.com>
2016-04-26 09:55:32 +0800

    Signed-off-by: XuminRong <xumingxsh21@126.com> 添加DataTable的Json转换,并添加单元测试代码

--- HiCSUtil/Json.cs
+++ HiCSUtil/Json.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Data;
 using System.IO;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Runtime.Serialization.Json;
 
 namespace HiCSUtil
@@ -51,6 +53,201 @@ namespace HiCSUtil
                 }
             }
         }
+
+        /// <summary>
+        /// 将json转换为DataTable
+        /// </summary>
+        /// <param name="strJson">得到的json</param>
+        /// <returns></returns>
+        public static DataTable Json2DataTable(string strJson)
+        {
+            //转换json格式
+            strJson = strJson.Replace(",\"", "*\"").Replace("\":", "\"#").ToString();
+            //取出表名   
+            var rg = new Regex(@"(?<={)[^:]+(?=:\[)", RegexOptions.IgnoreCase);
+            string strName = rg.Match(strJson).Value;
+            DataTable tb = null;
+            //去除表名   
+            strJson = strJson.Substring(strJson.IndexOf("[") + 1);
+            strJson = strJson.Substring(0, strJson.IndexOf("]"));
+
+            //获取数据   
+            rg = new Regex(@"(?<={)[^}]+(?=})");
+            MatchCollection mc = rg.Matches(strJson);
+            for (int i = 0; i < mc.Count; i++)
+            {
+                string strRow = mc[i].Value;
+                string[] strRows = strRow.Split('*');
+
+                //创建表   
+                if (tb == null)
+                {
+                    tb = new DataTable();
+                    tb.TableName = strName;
+                    foreach (string str in strRows)
+                    {
+                        var dc = new DataColumn();
+                        string[] strCell = str.Split('#');
+
+                        if (strCell[0].Substring(0, 1) == "\"")
+                        {
+                            int a = strCell[0].Length;
+                            dc.ColumnName = strCell[0].Substring(1, a - 2);
+                        }
+                        else
+                        {
+                            dc.ColumnName = strCell[0];
+                        }
+                        tb.Columns.Add(dc);
+                    }
+                    tb.AcceptChanges();
+                }
+
+                //增加内容   
+                DataRow dr = tb.NewRow();
+                for (int r = 0; r < strRows.Length; r++)
+                {
+                    dr[r] = strRows[r].Split('#')[1].Trim().Replace("，", ",").Replace("：", ":").Replace("\"", "");
+                }
+                tb.Rows.Add(dr);
+                tb.AcceptChanges();
+            }
+
+            if (tb.Rows.Count == 1)
+            {
+                bool isZeroRow = true;
+                foreach(object it in tb.Rows[0].ItemArray)
+                {
+                    if (!string.IsNullOrWhiteSpace(Convert.ToString(it)))
+                    {
+                        isZeroRow = false;
+                        break;
+                    }
+                }
+
+                if (isZeroRow)
+                {
+                    tb.Rows.Clear();
+                }
+            }
+            return tb;
+        }
+
+
+        /// <summary>     
+        /// Datatable转换为Json     
+        /// </summary>    
+        /// <param name="table">Datatable对象</param>     
+        /// <returns>Json字符串</returns>     
+        public static string DataTable2Json(DataTable dt)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("[");
+            DataRowCollection drc = dt.Rows;
+            if (drc.Count < 1)
+            {
+                sb.Append("{");
+                for (int i = 0; i < dt.Columns.Count; i++)
+                {
+                    if (i > 0)
+                    {
+                        sb.Append(",");
+                    }
+                    sb.AppendFormat("\"{0}\":\"\"", dt.Columns[i].ColumnName);
+                }
+                sb.Append("}]");
+                return sb.ToString();
+            }
+            for (int i = 0; i < drc.Count; i++)
+            {
+                if (i > 0)
+                {
+                    sb.Append(",");
+                }
+                sb.Append("{");
+                for (int j = 0; j < dt.Columns.Count; j++)
+                {
+					if (j > 0)
+					{
+						sb.Append(",");
+					}
+                    string strKey = dt.Columns[j].ColumnName;
+                    string strValue = drc[i][j].ToString();
+                    Type type = dt.Columns[j].DataType;
+                    sb.Append("\"" + strKey + "\":");
+                    strValue = StringFormat(strValue, type);
+                    sb.Append(strValue);
+                }
+                sb.Append("}");
+            }
+            sb.Remove(sb.Length - 1, 1);
+            sb.Append("]");
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// 格式化字符型、日期型、布尔型
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        private static string StringFormat(string str, Type type)
+        {
+            if (type == typeof(string))
+            {
+                str = String2Json(str);
+                str = "\"" + str + "\"";
+            }
+            else if (type == typeof(DateTime))
+            {
+                str = "\"" + str + "\"";
+            }
+            else if (type == typeof(bool))
+            {
+                str = str.ToLower();
+            }
+            else if (type != typeof(string) && string.IsNullOrEmpty(str))
+            {
+                str = "\"" + str + "\"";
+            }
+            return str;
+        }
+        /// <summary>
+        /// 过滤特殊字符
+        /// </summary>
+        /// <param name="s">字符串</param>
+        /// <returns>json字符串</returns>
+        private static string String2Json(String s)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < s.Length; i++)
+            {
+                char c = s.ToCharArray()[i];
+                switch (c)
+                {
+                    case '\"':
+                        sb.Append("\\\""); break;
+                    case '\\':
+                        sb.Append("\\\\"); break;
+                    case '/':
+                        sb.Append("\\/"); break;
+                    case '\b':
+                        sb.Append("\\b"); break;
+                    case '\f':
+                        sb.Append("\\f"); break;
+                    case '\n':
+                        sb.Append("\\n"); break;
+                    case '\r':
+                        sb.Append("\\r"); break;
+                    case '\t':
+                        sb.Append("\\t"); break;
+                    default:
+                        sb.Append(c); break;
+                }
+            }
+            return sb.ToString();
+        }
+    
 
     }
 }
---
+++ HiCSUtil/Test/UnitTestJson.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using HiCSUtil;
+
+namespace HiCSUtil.Test
+{
+    [TestClass]
+    public class UnitTestJson
+    {
+        [TestMethod]
+        public void UnitTestJson_DataTable()
+        {
+            DataTable dt = GetTable();
+            dt.TableName = "table";
+            string str = Json.DataTable2Json(dt);
+            System.Diagnostics.Debug.WriteLine(str);
+            DataTable dt2 = Json.Json2DataTable(str);
+            Assert.IsTrue(dt2.Rows.Count > 0);
+        }
+
+        [TestMethod]
+        public void UnitTestJson_DataTable2()
+        {
+            DataTable dt = GetTable(false);
+            dt.TableName = "table";
+            string str = Json.DataTable2Json(dt);
+            System.Diagnostics.Debug.WriteLine(str);
+            DataTable dt2 = Json.Json2DataTable(str);
+            Assert.IsTrue(dt2.Columns.Count > 0);
+        }
+
+        public DataTable GetTable(bool hasRow = true)
+        {
+            DataTable dt = GetDataRow();
+            if (!hasRow)
+            {
+                return dt;
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                DataRow dr = dt.NewRow();
+                dt.Rows.Add(dr);
+                SetData(dr);
+            }
+
+            return dt;
+        }
+
+        private DataTable GetDataRow()
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Str");
+            dt.Columns.Add("DT_val");
+            dt.Columns.Add("DTNull_val");
+            dt.Columns.Add("Int32_val");
+            dt.Columns.Add("Int32Null_val");
+            dt.Columns.Add("Int16_val");
+            dt.Columns.Add("Int64_val");
+            dt.Columns.Add("UInt32_val");
+            dt.Columns.Add("UInt16_val");
+            dt.Columns.Add("UInt64_val");
+            dt.Columns.Add("Double_val");
+            dt.Columns.Add("Decimal_val");
+            dt.Columns.Add("Float_val");
+            return dt;
+        }
+
+        private void SetData(DataRow dr)
+        {
+
+            dr["str"] = "test";
+            dr["dTNull_val"] = DBNull.Value;
+            dr["Int32_val"] = 3;
+            dr["int32Null_val"] = "";
+            dr["Int16_val"] = 2;
+            dr["Int64_val"] = "1";
+            dr["UInt32_val"] = 5;
+            dr["UInt16_val"] = 7;
+            dr["UInt64_val"] = 23;
+            dr["Double_val"] = 9;
+            dr["Decimal_val"] = "7";
+            dr["Float_val"] = "67";
+            dr["DT_val"] = "2015-02-03";
+        }
+    }
+}
